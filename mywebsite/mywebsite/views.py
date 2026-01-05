738fa793d55dde62d59b7ccb503174c3fcb7a4c5
Mahadiur <almahadiurrahman@gmail.com>
2026-01-05 07:25:54 -0800

    Fatch data in admin to frontend

--- mywebsite/mywebsite/views.py
+++ mywebsite/mywebsite/views.py
@@ -1,5 +1,11 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from Employee.models import employee
 
 def home(request):
-    return render(request, 'home.html')
+    emploee_list = employee.objects.all()
+    employee_context = {
+        'employees': emploee_list
+    }
+    print(employee_context)
+    return render(request, 'home.html', employee_context)
